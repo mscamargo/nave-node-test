38e67bfb380c97976a8296d8e87a9a54d56dc67c
mscamargo <mscamargo.dev@gmail.com>
2019-07-22 20:37:30 -0300

    feat: added auth middleware for some routes

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,19 @@ const routes = require('express').Router()
 const asyncWrap = require('./utils/async-wrap')
 
 const UserController = require('./controllers/UserController')
+const AuthenticationController = require('./controllers/AuthenticationController')
 const UserValidator = require('./validators/UserValidator')
+
+const authMiddleware = require('./middlewares/auth')
+
+routes.post('/login', asyncWrap(AuthenticationController.store))
 
 routes
   .route('/users')
-  .get(UserController.index)
+  .get(authMiddleware, UserController.index)
   .post(UserValidator.store(), asyncWrap(UserController.store))
 
+routes.use(authMiddleware)
 routes
   .route('/users/:id')
   .get(asyncWrap(UserController.show))
