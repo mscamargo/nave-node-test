19cb4ce0a0cff898b53c9c75c1f319e4f71e3b23
mscamargo <mscamargo.dev@gmail.com>
2019-07-23 23:11:48 -0300

    tests: added posts test

--- __tests__/integration/posts.test.js
+++ __tests__/integration/posts.test.js
@@ -1,10 +1,137 @@
+const request = require('supertest')
+const faker = require('faker')
+
+const app = require('../../src/app')
+const truncateTables = require('../utils/truncate-tables')
+const factory = require('../utils/factories')
+const jwt = require('../../src/utils/jwt')
 
 describe('Posts Resource', () => {
-  describe('GET /posts')
+  beforeEach(async () => {
+    await truncateTables()
+  })
+  describe('GET /posts', () => {
+    it('should be able to view all posts if authenticated', async () => {
+      const user = await factory.create('User')
 
-  describe('POST /posts')
+      const token = jwt.generate(user)
 
-  describe('PUT /posts')
+      const response = await request(app).get('/posts')
+        .set('Authorization', `Bearer ${token}`)
+        .send()
 
-  describe('DELETE /posts')
+      expect(response.status).toEqual(200)
+    })
+
+    it('should not be able to view all posts if not authenticated', async () => {
+      const response = await request(app).get('/posts').send()
+
+      expect(response.status).toEqual(401)
+    })
+
+    it('should be in descending order of date', async () => {
+      const user = await factory.create('User')
+      await factory.createMany('Post', 5, { user_id: user.id })
+
+      const token = jwt.generate(user)
+
+      const { body } = await request(app)
+        .get('/posts')
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+
+      const dates = body.map(post => new Date(post.createdAt))
+
+      expect(dates[0].getTime()).toBeLessThanOrEqual(dates[0].getTime())
+    })
+  })
+
+  describe('POST /posts', () => {
+    it('should be able to create a post', async () => {
+      const user = await factory.create('User')
+
+      const token = jwt.generate(user)
+
+      const response = await request(app)
+        .post('/posts')
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          text: faker.lorem.sentence()
+        })
+
+      expect(response.status).toEqual(201)
+    })
+  })
+
+  describe('PUT /posts', () => {
+    it('should be able to update your post', async () => {
+      const user = await factory.create('User')
+      const post = await factory.create('Post', { user_id: user.id })
+
+      const token = jwt.generate(user)
+
+      const response = await request(app)
+        .put(`/posts/${post.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          text: faker.lorem.sentence()
+        })
+
+      expect(response.status).toEqual(200)
+    })
+
+    it('should not be able to update post of other users', async () => {
+      const users = await factory.createMany('User', 2, [
+        { username: 'user0', email: 'user0@email.com' },
+        { username: 'user1', email: 'user1@email.com' }
+      ])
+
+      const post = await factory.create('Post', { user_id: users[1].id })
+
+      const token = jwt.generate(users[0])
+
+      const response = await request(app)
+        .put(`/posts/${post.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send({
+          text: faker.lorem.sentence()
+        })
+
+      expect(response.status).toEqual(403)
+    })
+  })
+
+  describe('DELETE /posts', () => {
+    it('should be able to delete your post', async () => {
+      const user = await factory.create('User')
+      const post = await factory.create('Post', { user_id: user.id })
+
+      const token = jwt.generate(user)
+
+      const response = await request(app)
+        .delete(`/posts/${post.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+
+      expect(response.status).toEqual(204)
+    })
+
+    it('should not be able to delete post of other users', async () => {
+      const users = await factory.createMany('User', 2, [
+        { username: 'user0', email: 'user0@email.com' },
+        { username: 'user1', email: 'user1@email.com' }
+      ])
+
+      const post = await factory.create('Post', { user_id: users[1].id })
+
+      const token = jwt.generate(users[0])
+
+      const response = await request(app)
+        .delete(`/posts/${post.id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+
+      expect(response.status).toEqual(403)
+    })
+  })
 })
--- __tests__/utils/factories.js
+++ __tests__/utils/factories.js
@@ -1,12 +1,16 @@
 const faker = require('faker')
 const { factory } = require('factory-girl')
-const { User } = require('../../src/models')
+const { User, Post } = require('../../src/models')
 
 factory.define('User', User, {
   username: faker.internet.userName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
   title: faker.name.findName()
+})
+
+factory.define('Post', Post, {
+  text: faker.lorem.sentence()
 })
 
 module.exports = factory
